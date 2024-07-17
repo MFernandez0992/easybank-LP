0539e97fee2ac79782eae6439756a27692e680a3
Matías Fernández <61636811+MFernandez0992@users.noreply.github.com>
2024-07-17 16:28:06 -0300

    Responsive mobile finalizado
    Resta ajustar detalles para resoluciones bajas

---
+++ easybank/src/App.jsx
@@ -0,0 +1,20 @@
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Blog />
+      <Footer />
+    </>
+  )
+}
+
+export default App
