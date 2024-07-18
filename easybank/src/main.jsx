adb0a4f3eb1f9e0420cac7f183d5f44d3abb4cbd
Matías Fernández <61636811+MFernandez0992@users.noreply.github.com>
2024-07-18 12:54:29 -0300

    Update main.jsx

--- easybank/src/main.jsx
+++ easybank/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Header />
+      <Hero />
+      <About />
+      <Blog />
+      <Footer />
   </React.StrictMode>,
 )
